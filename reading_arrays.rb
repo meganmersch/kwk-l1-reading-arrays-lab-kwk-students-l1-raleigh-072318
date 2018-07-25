2ca1d65287548bc6888ab22aaa5b277473c51223
meganmersch <meganrm8@gmail.com>
2018-07-25 17:50:09 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -29,6 +29,7 @@ def first_student_by_method
   puts student_names.first
 end
 first_student_by_method
+
 def last_student_by_method
   # Write a solution that returns the first student using the built in .last method
 end
